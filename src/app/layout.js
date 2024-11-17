bd9c1c50a57ac8180fcfb83bc4e6ea7b0e67a6f3
stefanRachev <starfystef@gmail.com>
2024-11-17 22:34:53 +0200

    install next-auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "@/components/auth/Navbar";
+Navbar;
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,6 +25,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
