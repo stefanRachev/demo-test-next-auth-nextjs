f480bafe3a510b0e78deded3ae355b13b43a9f79
stefanRachev <starfystef@gmail.com>
2024-11-21 22:46:58 +0200

    add logout functionality

--- src/components/auth/Navbar.jsx
+++ src/components/auth/Navbar.jsx
@@ -1,32 +1,52 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { auth, signOut } from "@/auth";
 
+const Navbar = async () => {
+  const session = await auth();
+  const user = session?.user;
 
-const Navbar = () => {
   return (
     <nav className="flex justify-around items-center py-4 bg-gray-600 text-white">
-      <Link href="/" className="text-xl font-bold">My Fancy Website</Link>
+      <Link href="/" className="text-xl font-bold">
+        My Fancy Website
+      </Link>
 
       <ul className="hidden md:flex space-x-4 list-none ">
-        <li>
-          <Link href="/login" className="hover:text-gray-400">
-            Login
-          </Link>
-        </li>
-        <li>
-          <Link href="/register" className="hover:text-gray-400">
-            Register
-          </Link>
-        </li>
-        <li>
-          <Link href="/private/dashboard" className="hover:text-gray-400">
-            Dashboard
-          </Link>
-        </li>
+        {!user ? (
+          <>
+            <li>
+              <Link href="/login" className="hover:text-gray-400">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link href="/register" className="hover:text-gray-400">
+                Register
+              </Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li className="mt-2">
+              <Link href="/private/dashboard" className="hover:text-gray-400">
+                Dashboard
+              </Link>
+            </li>
 
-        <form>
-          <Button type="submit" variant={"ghost"}>Logout</Button>
-        </form>
+            <form
+              action={async () => {
+                "use server";
+
+                await signOut();
+              }}
+            >
+              <Button type="submit" variant={"ghost"}>
+                Logout
+              </Button>
+            </form>
+          </>
+        )}
       </ul>
     </nav>
   );
